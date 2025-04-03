0a7603cacaa54ee520e2f802d240d9f0314fa262
alvarocimadevilla <alvarocimadevilla.trabajos@gmail.com>
2025-04-03 09:46:49 +0200

    Migrados cambios desde reservas-frontend

--- client/src/app/components/home/layout/main/FirstWindow/first-window.component.ts
+++ client/src/app/components/home/layout/main/FirstWindow/first-window.component.ts
@@ -7,12 +7,18 @@ import { CommonModule } from '@angular/common';
 import { CalendarioComponent } from "../minicomponents/calendario/calendario.component";
 import { ZonaHorariaComponent } from '../minicomponents/zona-horaria/zona-horaria.component';
 import { HorasReservaComponent } from '../minicomponents/horas-reserva/horas-reserva.component';
-import { HeaderComponent } from "../../Header/header.component";
-import { FooterComponent } from "../../Footer/footer.component";
-
 @Component({
   selector: 'app-first-window',
-  imports: [MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatButtonModule, CommonModule, CalendarioComponent, ZonaHorariaComponent, HorasReservaComponent],
+  imports: [
+    CommonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    CalendarioComponent,
+    ZonaHorariaComponent,
+    HorasReservaComponent,
+  ],
   templateUrl: './first-window.component.html',
 })
 export class FirstWindowComponent {
