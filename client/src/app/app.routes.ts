b9da52a6256391823ffbb9d9024b1243b247c96a
Carlos Martín Salvatierra <carlos.martin33@educa.madrid.org>
2025-04-08 12:58:07 +0200

    Commit 25 app routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardAdminComponent } from './components/admin/dashboard/dashboard-admin.component';
+import { LoginWindowComponent } from './components/login/login-window/login-window.component';
+import { RecoveryWindowComponent } from './components/login/recovery-window/recovery-window.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Ruta principal
@@ -10,5 +12,7 @@ export const routes: Routes = [
   { path: 'admin/employees', component: DashboardAdminComponent }, // Dashboard con empleados
   { path: 'admin/reports', component: DashboardAdminComponent }, // Dashboard con reportes
   { path: 'admin/settings', component: DashboardAdminComponent }, // Dashboard con configuración
+  { path: 'login', component: LoginWindowComponent },
+  { path: 'recovery', component: RecoveryWindowComponent },
   { path: '**', redirectTo: '' } // Redirección para rutas no encontradas
 ];
