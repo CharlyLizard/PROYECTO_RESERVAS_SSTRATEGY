7124bbecff28468f9d08f77957356c643e029610
alvarocimadevilla <alvarocimadevilla.trabajos@gmail.com>
2025-05-30 10:39:36 +0200

    Commit 109 arreglado footer

--- client/src/app/components/home/layout/Footer/footer.component.ts
+++ client/src/app/components/home/layout/Footer/footer.component.ts
@@ -1,28 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { LanguageService, Language } from '../../../../services/language.service';
 import { TranslatePipe } from '../../../../pipe/translate.pipe';
-=======
-import { CommonModule } from '@angular/common'; // Para *ngFor
-import { RouterLink } from '@angular/router'; // Importar RouterLink
-import { LanguageService, Language } from '../../../../services/language.service'; // Tu servicio de idioma
-import { TranslatePipe } from '../../../../pipe/translate.pipe'; // Tu pipe de traducción personalizada
->>>>>>> fe71ad993a86c23b9c0e78d4359e4247e7cad7aa
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   standalone: true,
   imports: [
-<<<<<<< HEAD
     CommonModule,
-    TranslatePipe
-=======
-    CommonModule,    // Necesario para directivas como *ngFor
-    RouterLink,      // Añadir RouterLink para la directiva routerLink
-    TranslatePipe    // Tu pipe de traducción personalizada
->>>>>>> fe71ad993a86c23b9c0e78d4359e4247e7cad7aa
+    TranslatePipe,    RouterLink,
   ]
 })
 export class FooterComponent implements OnInit {
